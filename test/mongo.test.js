0a5cbc5ba23920a61438391451ea3c9a2c6a6047
James Sumners <james.sumners@gmail.com>
2018-03-08 20:04:24 -0500

    Add integration test with abstract-cache-mongo

---
+++ test/mongo.test.js
@@ -0,0 +1,75 @@
+'use strict'
+
+const test = require('tap').test
+const request = require('request')
+const fastify = require('fastify')
+const fastifyCaching = require('fastify-caching')
+const fastifyCookie = require('fastify-cookie')
+const plugin = require('../')
+const secretKey = '12345678901234567890123456789012'
+
+test('can store sessions in a mongo database', (t) => {
+  t.plan(3)
+
+  const server = fastify()
+  const cache = require('abstract-cache')({
+    useAwait: false,
+    driver: {
+      name: 'abstract-cache-mongo',
+      options: {
+        dbName: 'testing',
+        mongodb: {
+          url: 'mongodb://localhost:27017/testing'
+        }
+      }
+    }
+  })
+
+  t.tearDown(() => {
+    server.close()
+    cache.stop(() => {})
+  })
+
+  cache.start((err) => {
+    if (err) t.threw(err)
+    runTests()
+  })
+
+  function runTests () {
+    server
+      .register(fastifyCookie)
+      .register(fastifyCaching, {cache})
+      .register(plugin, {secretKey})
+      .after((err) => {
+        if (err) t.threw(err)
+      })
+
+    server.get('/one', (req, reply) => {
+      t.ok(req.session)
+      t.deepEqual(req.session, {})
+      req.session.one = true
+      reply.send()
+    })
+
+    server.get('/two', (req, reply) => {
+      t.strictDeepEqual(req.session, {
+        one: true
+      })
+      reply.send()
+    })
+
+    server.listen(0, (err) => {
+      server.server.unref()
+      if (err) t.threw(err)
+      const port = server.server.address().port
+      const address = `http://127.0.0.1:${port}`
+      const jar = request.jar()
+      request.get(`${address}/one`, {jar}, (err, res, body) => {
+        if (err) t.threw(err)
+        request.get(`${address}/two`, {jar}, (err, res, body) => {
+          if (err) t.threw(err)
+        })
+      })
+    })
+  }
+})
