2e77115bccd5ba4dcf0bb57d0740f5a1f4aba6f5
Vincent LE GOFF <vince.legoff@gmail.com>
2021-09-03 11:05:35 +0200

    feat: fix use await hanging issue

--- plugin.js
+++ plugin.js
@@ -41,6 +41,21 @@ function plugin (fastify, options, pluginRegistrationDone) {
 
   fastify.decorateRequest('session', null)
   fastify.addHook('onRequest', function (req, reply, hookFinished) {
+    function getHandler (cached) {
+      if (!cached) {
+        req.log.trace('session data missing (new/expired)')
+        return hookFinished()
+      }
+      req.session = getSession(cached.item)
+      req.log.trace('session restored: %j', req.session)
+      hookFinished()
+    }
+
+    function getErrorHandler (err) {
+      req.log.trace('could not retrieve session data')
+      hookFinished(err)
+    }
+
     req.session = getSession()
     if (!req.cookies[opts.sessionCookieName]) {
       return hookFinished()
@@ -56,19 +71,19 @@ function plugin (fastify, options, pluginRegistrationDone) {
       return hookFinished()
     }
 
-    this.cache.get(sessionId, (err, cached) => {
-      if (err) {
-        req.log.trace('could not retrieve session data')
-        return hookFinished(err)
-      }
-      if (!cached) {
-        req.log.trace('session data missing (new/expired)')
-        return hookFinished()
-      }
-      req.session = getSession(cached.item)
-      req.log.trace('session restored: %j', req.session)
-      hookFinished()
-    })
+    if (this.cache.await) {
+      this.cache.get(sessionId)
+        .then(getHandler)
+        .catch(getErrorHandler)
+    } else {
+      this.cache.get(sessionId, (err, cached) => {
+        if (err) {
+          getErrorHandler(err)
+        } else {
+          getHandler(cached)
+        }
+      })
+    }
   })
 
   fastify.addHook('onSend', function (req, reply, payload, hookFinished) {
@@ -84,21 +99,7 @@ function plugin (fastify, options, pluginRegistrationDone) {
     uidgen(18, storeSession.bind(this))
 
     function storeSession (err, sessionId) {
-      if (err) {
-        req.log.trace('could not store session with invalid id')
-        return hookFinished(err)
-      }
-
-      if (!sessionId) {
-        req.log.trace('could not store session with missing id')
-        return hookFinished(Error('missing session id'))
-      }
-
-      this.cache.set(sessionId, req.session, opts.sessionMaxAge, err => {
-        if (err) {
-          req.log.trace('error saving session: %s', err.message)
-          return hookFinished(err)
-        }
+      function cacheSetHandler () {
         const cookieExiresMs = opts.cookie && opts.cookie.expires
         const cookieOpts = merge({}, opts.cookie, {
           expires: !cookieExiresMs
@@ -108,7 +109,35 @@ function plugin (fastify, options, pluginRegistrationDone) {
         const signedId = sign(sessionId, opts.secretKey)
         reply.setCookie(opts.sessionCookieName, signedId, cookieOpts)
         hookFinished()
-      })
+      }
+
+      function cacheErrorHandler () {
+        req.log.trace('error saving session: %s', err.message)
+        return hookFinished(err)
+      }
+
+      if (err) {
+        req.log.trace('could not store session with invalid id')
+        return hookFinished(err)
+      }
+
+      if (!sessionId) {
+        req.log.trace('could not store session with missing id')
+        return hookFinished(Error('missing session id'))
+      }
+      if (this.cache.await) {
+        this.cache.set(sessionId, req.session, opts.sessionMaxAge)
+          .then(cacheSetHandler)
+          .catch(cacheErrorHandler)
+      } else {
+        this.cache.set(sessionId, req.session, opts.sessionMaxAge, err => {
+          if (err) {
+            cacheErrorHandler(err)
+          } else {
+            cacheSetHandler()
+          }
+        })
+      }
     }
   })
 
